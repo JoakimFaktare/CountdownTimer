a3a30786c8b934b97f00718d78367cce7bf5326c
JoakimFaktare <joakim.schroeder@live.se>
2022-12-29 14:37:28 +0100

    First push

---
+++ Countdown_timer.py
@@ -0,0 +1,141 @@
+import time
+from tkinter import *
+from tkinter import PhotoImage
+import winsound
+from tkinter import messagebox
+
+# creating Tk window
+root = Tk()
+
+# setting geometry of tk window
+root.geometry("400x230+500+50")
+
+root.resizable(0, 0)
+
+# Giving the window some color
+root.configure(bg="purple")
+# Adding columns
+root.columnconfigure(0, weight=2)
+root.columnconfigure(1, weight=2)
+root.columnconfigure(2, weight=2)
+root.columnconfigure(3, weight=2)
+root.columnconfigure(4, weight=2)
+
+
+# Using title() to display a message in
+# the dialogue box of the message in the
+# title bar.
+root.title("Time Counter")
+
+# Declaration of variables
+hour = StringVar()
+minute = StringVar()
+second = StringVar()
+message = StringVar()
+
+# setting the default value as 0
+hour.set("00")
+minute.set("00")
+second.set("00")
+
+# Use of Entry class to take input from the user
+hourEntry_label = Label(root, text="Hour")
+hourEntry_label.grid(column=1, row=0, sticky=E)
+hourEntry = Entry(root, width=2, font=("Arial", 18, ""),
+                  textvariable=hour)
+hourEntry.grid(column=1, row=1, sticky=E)
+
+minuteEntry_label = Label(root, text="Minutes")
+minuteEntry_label.grid(column=2, row=0, pady=10)
+minuteEntry = Entry(root, width=2, font=("Arial", 18, ""),
+                    textvariable=minute)
+minuteEntry.grid(column=2, row=1)
+
+secondEntry_label = Label(root, text="Seconds")
+secondEntry_label.grid(column=3, row=0 ,sticky=W)
+secondEntry = Entry(root, width=2, font=("Arial", 18, ""),
+                    textvariable=second)
+secondEntry.grid(column=3, row=1, sticky=W)
+
+# field to enter message you want to show up in pop up window when time is up
+messageEntry = Entry(root, width=10, font=("Arial", 18, ""),
+                     textvariable=message)
+messageEntry.grid(column=2, row=6, pady=10, padx=5)
+
+#Pop-up window when time is up
+def open_topLevel():
+    winsound.PlaySound("alarm.wav", winsound.SND_ASYNC)
+    top = Toplevel()
+    #Changing background color just for fun.
+    top.configure(bg="LightPink1")
+    # Using zoomed to make the pop-up window maximized
+    top.state("zoomed")
+    top.title("Finished")
+    top.bgPic = PhotoImage(file="deadpoolBG.gif")  # Assign the image to an attribute of the Toplevel widget
+    top_background = Label(top, image=top.bgPic)
+    top_background.pack()
+    top_label = Label(top, text="Time's up! \n\n" + message.get(), font=("Helvetica", 20, "bold"),
+                      fg="#8B0000")
+    #Button to go back to main window.
+    top_btn = Button(top, text="Thank you!", command=top.destroy)
+    top_label.pack()
+    top_btn.pack()
+
+
+
+
+def submit():
+    try:
+        # the input provided by the user is
+        # stored in here :temp
+        temp = int(hour.get()) * 3600 + int(minute.get()) * 60 + int(second.get())
+    except:
+        print("Please input the right value")
+
+    while temp > -1:
+
+        # divmod(firstvalue = temp//60, secondvalue = temp%60)
+        mins, secs = divmod(temp, 60)
+
+        # Converting the input entered in mins or secs to hours,
+        # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+        # 50min: 0sec)
+        hours = 0
+        if mins > 59:
+            # divmod(firstvalue = temp//60, secondvalue
+            # = temp%60)
+            hours, mins = divmod(mins, 60)
+
+        # using format () method to store the value up to
+        # two decimal places
+        hour.set("{0:2d}".format(hours))
+        minute.set("{0:2d}".format(mins))
+        second.set("{0:2d}".format(secs))
+
+        # updating the GUI window after decrementing the
+        # temp value every time
+        root.update()
+        time.sleep(1)
+
+        # when temp value = 0; then a messagebox pop's up
+        # with a message:"Time's up"
+        # and message from user input
+        if (temp == 0):
+            #messagebox.showinfo("Time Countdown", "Time's up \n" + message.get())
+            open_topLevel()
+
+        # after every one sec the value of temp will be decremented
+        # by one
+        temp -= 1
+
+
+# button widget
+btn = Button(root, text='Set Time Countdown', bd='5',
+             command=submit)
+btn.grid(column=2, row=8, padx=5, pady=5)
+
+
+# infinite loop which is required to
+# run tkinter program infinitely
+# until an interrupt occurs
+root.mainloop()
