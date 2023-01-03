6abdfb4115d9499bf0d1fb389d8a48791e3b73ad
JoakimFaktare <joakim.schroeder@live.se>
2023-01-03 11:40:40 +0100

    Added "Reset Timer" button and functionality to stop and reset timer to 0

--- Countdown_timer.py
+++ Countdown_timer.py
@@ -2,6 +2,7 @@ import time
 from tkinter import *
 from tkinter import PhotoImage
 import winsound
+import threading
 from tkinter import messagebox
 
 # creating Tk window
@@ -20,7 +21,6 @@ root.columnconfigure(1, weight=2)
 root.columnconfigure(2, weight=2)
 root.columnconfigure(3, weight=2)
 root.columnconfigure(4, weight=2)
-
 
 # Using title() to display a message in
 # the dialogue box of the message in the
@@ -52,7 +52,7 @@ minuteEntry = Entry(root, width=2, font=("Arial", 18, ""),
 minuteEntry.grid(column=2, row=1)
 
 secondEntry_label = Label(root, text="Seconds")
-secondEntry_label.grid(column=3, row=0 ,sticky=W)
+secondEntry_label.grid(column=3, row=0, sticky=W)
 secondEntry = Entry(root, width=2, font=("Arial", 18, ""),
                     textvariable=second)
 secondEntry.grid(column=3, row=1, sticky=W)
@@ -62,11 +62,13 @@ messageEntry = Entry(root, width=14, font=("Arial", 18, ""),
                      textvariable=message)
 messageEntry.grid(column=2, row=6, pady=10, padx=5)
 
-#Pop-up window when time is up
+
+# Pop-up window when time is up
 def open_topLevel():
+    # Using winsound to play a sound when TopLevel window appears
     winsound.PlaySound("alarm.wav", winsound.SND_ASYNC)
     top = Toplevel()
-    #Changing background color just for fun.
+    # Changing background color just for fun.
     top.configure(bg="LightPink1")
     # Using zoomed to make the pop-up window maximized
     top.state("zoomed")
@@ -76,28 +78,38 @@ def open_topLevel():
     top_background.pack()
     top_label = Label(top, text="Time's up!", font=("Helvetica", 20, "bold"), bg="LightPink1",
                       fg="#8B0000")
-    #Another label to display the message from the input window
+    # Another label to display the message from the input window
     top_label_message = Label(top, text=message.get(), font=("Helvetica", 20, "bold", "underline"), bg="LightPink1",
                               fg="#8B0000")
-    #Button to go back to main window.
+    # Button to go back to main window.
     top_btn = Button(top, text="Thank you!", borderwidth=3, relief="raised", font=("Helvetica", 15),
                      command=top.destroy)
     top_label.pack(pady=5)
     top_label_message.pack(pady=5)
     top_btn.pack(pady=10)
 
+#Variable set to false to be able to stop thread later
+stop_thread = False
 
+#Function to reset timer down to zero and stop the timer
+def reset_timer():
+    global stop_thread
+    stop_thread = True
+    hour.set("00")
+    minute.set("00")
+    second.set("00")
 
-
+#Function for the timer itself with input from user
 def submit():
+    global stop_thread
+    stop_thread = False
     try:
         # the input provided by the user is
         # stored in here :temp
         temp = int(hour.get()) * 3600 + int(minute.get()) * 60 + int(second.get())
     except:
         print("Please input the right value")
-
-    while temp > -1:
+    while not stop_thread and temp != -1:
 
         # divmod(firstvalue = temp//60, secondvalue = temp%60)
         mins, secs = divmod(temp, 60)
@@ -121,12 +133,10 @@ def submit():
         # temp value every time
         root.update()
         time.sleep(1)
-
         # when temp value = 0; then a messagebox pop's up
-        # with a message:"Time's up"
         # and message from user input
-        if (temp == 0):
-            #messagebox.showinfo("Time Countdown", "Time's up \n" + message.get())
+        if temp == 0:
+            # messagebox.showinfo("Time Countdown", "Time's up \n" + message.get())
             open_topLevel()
 
         # after every one sec the value of temp will be decremented
@@ -135,10 +145,12 @@ def submit():
 
 
 # button widget
-btn = Button(root, text='Set Time Countdown', bd='5',
-             command=submit)
-btn.grid(column=2, row=8, padx=5, pady=5)
-
+start_btn = Button(root, text='Set Time Countdown', bd='5',
+                   command=submit)
+start_btn.grid(column=2, row=8, padx=5, pady=5)
+#Button to stop the countdown and reset the timer
+reset_button = Button(root, text="Reset Timer", command=reset_timer)
+reset_button.grid(column=2, row=9, padx=5, pady=5)
 
 # infinite loop which is required to
 # run tkinter program infinitely
